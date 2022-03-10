70b8ff38509815af7ea246c499f65d58513fa9bd
aniketmpatil <aniketp1597@gmail.com>
2022-03-10 18:35:35 -0500

    test push

--- main.py
+++ main.py
@@ -40,15 +40,17 @@ if __name__ == "__main__":
     RRT_planner = RRT(map_array, start, goal)
 
     # Search with PRM
-    PRM_planner.sample(n_pts=1000, sampling_method="uniform")
-    PRM_planner.search(start, goal)
+    # PRM_planner.sample(n_pts=1000, sampling_method="uniform")
+    # PRM_planner.search(start, goal)
     PRM_planner.sample(n_pts=1000, sampling_method="random")
+    print(PRM_planner.check_collision([10, 10], [10, 250]))
+    print(PRM_planner.check_collision([150, 10], [150, 100]))
     PRM_planner.search(start, goal)
-    PRM_planner.sample(n_pts=2000, sampling_method="gaussian")
-    PRM_planner.search(start, goal)
-    PRM_planner.sample(n_pts=20000, sampling_method="bridge")
-    PRM_planner.search(start, goal)
+    # PRM_planner.sample(n_pts=2000, sampling_method="gaussian")
+    # PRM_planner.search(start, goal)
+    # PRM_planner.sample(n_pts=20000, sampling_method="bridge")
+    # PRM_planner.search(start, goal)
 
     # Search with RRT and RRT*
-    RRT_planner.RRT(n_pts=1000)
-    RRT_planner.RRT_star(n_pts=2000)
+    # RRT_planner.RRT(n_pts=1000)
+    # RRT_planner.RRT_star(n_pts=2000)
